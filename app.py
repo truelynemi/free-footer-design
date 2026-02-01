57f0ecf6014d486e2fd3281569b57aeaf0cd364c
YY <mightybj10@gmail.com>
2026-02-01 23:49:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,8 @@
 # app.py
 from flask import (Flask, render_template, request, redirect, url_for, flash, session)
-from werkzeug.security import generate_password_hash, check_password_hash
 
-
-# -------------------------------------------------
-# APP CONFIGURATION
-# -------------------------------------------------
 app = Flask(__name__)
 
-# Path to SQLite database file (created automatically if it doesn't exist)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rza.db'
-
-# Turn off a noisy feature we don't need
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Secret key is used for flashes + sessions (keep this private in real life)
-app.config['SECRET_KEY'] = 'a_long_bankai_key_i_suppose'
-
-
-
-# -------------------------------------------------
-# ROUTES FOR STATIC PAGES
-# -------------------------------------------------
 
 @app.route('/')
 def index():
